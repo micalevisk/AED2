23e5d0fa7a403bb930763de7430e838ce0106edc
Micael Levi <mllc@icomp.ufam.edu.br>
2016-09-11 21:55:12 -0400

    add:instancia testada

---
+++ trabalho2/testes/converterPA.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##
+##	[converterPA.sh]
+##	Created by Micael Levi on 09/11/2016
+##	Copyright (c) 2016 mllc@icomp.ufam.edu.br; All rights reserved.
+##
+
+
+gawk '
+{ a[i++] = $0 }
+END {
+	pag=1
+	for(j=0; j < i; ){
+		if( gsub(/PA /, "PAGINA:"pag"\n", a[j]) ) pag++
+		print a[j++]
+	}
+}' "$1"
